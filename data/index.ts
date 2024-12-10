033017b7da1fd86fc9e400f46e9790161af5971e
Rubal Banerjee <rubal@Rubals-MacBook-Air.local>
2024-12-10 06:53:59 +0530

    Final Commit

---
+++ data/index.ts
@@ -0,0 +1,206 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building projects on Golang",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Evently",
+    des: "A feature-rich event organization application.",
+    img: "/Evently.png",
+    iconLists: ["/next.svg", "/re.svg", "/tail.svg", "/ts.svg", "/c.svg"],
+    link: "https://event-organization-platform.vercel.app",
+  },
+  {
+    id: 2,
+    title: "Gamma",
+    des: "A modern website featuring sleek parallax effects and visually appealing bento box layouts.",
+    img: "/Gamma.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg"],
+    link: "https://gamma-phi.vercel.app",
+  },
+  {
+    id: 3,
+    title: "Techbuzzz",
+    des: "A production-grade e-commerce application built with Next.js framework.",
+    img: "/Techbuzzz.png",
+    iconLists: ["/next.svg", "/re.svg", "/tail.svg", "/ts.svg"],
+    link: "https://e-commerce-app-delta-nine.vercel.app",
+  },
+  {
+    id: 4,
+    title: "Mom's Recipe",
+    des: "A recipe finder application with state management.",
+    img: "/Mom's Recipes.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg"],
+    link: "https://food-recipe-app-green.vercel.app",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Rubal was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Rubal's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Rubal is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Rubal was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Rubal's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Rubal is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Rubal was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Rubal's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Rubal is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Rubal was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Rubal's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Rubal is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Rubal was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Rubal's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Rubal is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Mobile App Dev - Upwork",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 2,
+    title: "Freelance Web Dev Project",
+    desc: "Led the dev of a web app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/Rubal-Banerjee",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://x.com/Rubal44444",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://linkedin.com/in/rubal-banerjee",
+  },
+];
